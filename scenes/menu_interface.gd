e650a03870b9a55bdddd2331c134f5dce1a939ce
FeignNext <102315773+Hollowborn@users.noreply.github.com>
2026-03-07 17:27:51 +0800

    added start menu

---
+++ scenes/menu_interface.gd
@@ -0,0 +1,22 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_1_training_yard.tscn")
+
+func _on_settingsbutton_pressed() -> void:
+	print("Options menu not implemented yet")  # Replace with function body.
+
+
+func _on_exitbutton_pressed() -> void:
+	get_tree().quit() # Replace with function body.
