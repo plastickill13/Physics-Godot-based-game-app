3868b509375a970bd52ff0cf6a8c725ce6ceb285
FeignNext <102315773+Hollowborn@users.noreply.github.com>
2026-03-09 21:50:29 +0800

    minor fixes

---
+++ scenes/nowplaying.gd
@@ -0,0 +1,15 @@
+extends Label
+
+@export var scroll_speed: float = 40.0 # Pixels per second
+
+@onready var container: Control = get_parent()
+
+func _process(delta: float) -> void:
+	# 1. Slide the text from left to right
+	position.x += scroll_speed * delta
+	
+	# 2. Check if the text has completely left the right side of the container
+	# (position.x is the left edge of the text)
+	if position.x > container.size.x:
+		# Teleport it to the far left, just out of sight, so it scrolls back in
+		position.x = -size.x
